bbabdacd70fba8d361c63f815f121f63fd51cd3b
vivek <vivekneupane11@gmail.com>
2020-05-16 07:38:54 +0545

    Module Media queries

--- socialproof/js/socialproof.js
+++ socialproof/js/socialproof.js
@@ -1,14 +1,29 @@
 let count = 0;
 let timer = 0;
+var startTime = new Date().getTime();
+let isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
 window.onload = function() {
   jQuery('#mainsp').delay(4000).animate({opacity:1},3000);
 
+  jQuery('.closebutton').click(function(){
+      jQuery('#mainsp').animate({opacity:0,top:'100vh'},1000);
+ jQuery('#allmessagesdiv').delay(1000).animate({'marginTop':'0px'});
+    clearInterval(interval);
+  });
 function showmessage(){
     let lengthCounter = jQuery('#count').val()-1;
-    console.log(lengthCounter);
-    jQuery('#mainsp').animate({opacity:0},1000);
-    jQuery('#allmessagesdiv').delay(1000).animate({'marginTop':'-=75px'},2000);
-    jQuery('#mainsp').delay(2000).animate({opacity:1},1000);
+
+    jQuery('#mainsp').animate({opacity:0,top:'100vh'},1000);
+
+ if (isMobile) {
+   jQuery('#allmessagesdiv').delay(1000).animate({'marginTop':'-=82px'},2000);
+   jQuery('#mainsp').delay(2000).animate({opacity:1,top:'82vh'},1000);
+ }
+ else{
+   jQuery('#allmessagesdiv').delay(1000).animate({'marginTop':'-=122px'},2000);
+   jQuery('#mainsp').delay(2000).animate({opacity:1,top:'80vh'},1000);
+ }
+
       count++;
       if(count>lengthCounter){
           jQuery('#allmessagesdiv').animate({'marginTop':'0px'},10);
@@ -16,11 +31,13 @@ function showmessage(){
           timer+= 1;
           count = 0;
           if(timer >= 3){
-            jQuery('#mainsp').animate({opacity:0},1000);
-             jQuery('#allmessagesdiv').animate({'marginTop':'0px'},10);
+              jQuery('#mainsp').animate({opacity:0,top:'100vh'},1000);
+             jQuery('#allmessagesdiv').delay(1000).animate({'marginTop':'0px'});
             clearInterval(interval);
           }
       }
+
+
 }
   var interval = setInterval(showmessage,10000);
 }
